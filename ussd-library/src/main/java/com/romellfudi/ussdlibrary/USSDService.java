534ce7620209daa0f9205585009ba48f830a6f4f
abdelatif ouchene <85649039+abdelatifdz34@users.noreply.github.com>
2021-06-09 23:49:53 +0100

    Update USSDService.java

--- ussd-library/src/main/java/com/romellfudi/ussdlibrary/USSDService.java
+++ ussd-library/src/main/java/com/romellfudi/ussdlibrary/USSDService.java
@@ -153,7 +153,8 @@ public class USSDService extends AccessibilityService {
         return (event.getClassName().equals("amigo.app.AmigoAlertDialog")
                 || event.getClassName().equals("android.app.AlertDialog")
                 || event.getClassName().equals("com.android.phone.oppo.settings.LocalAlertDialog")
-                || event.getClassName().equals("com.zte.mifavor.widget.AlertDialog"));
+                || event.getClassName().equals("com.zte.mifavor.widget.AlertDialog"))
+                || event.getClassName().equals("color.support.v7.app.AlertDialog"));
     }
 
     /**
